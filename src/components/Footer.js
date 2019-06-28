05dc3fc4a7af6447903540f436c0cdf556e113f7
Darin Levesque <darinlevesque@gmail.com>
2019-06-28 00:48:31 -0400

    added dynamic last updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,53 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from "gatsby"
+// import PropTypes from 'prop-types'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
 
-const Footer = (props) => (
-    <footer
-        id="footer"
-        style={props.timeout
-        ? {
-            display: 'none'
-        }
-        : {}}>
+
+export default () => (
+        <StaticQuery
+          query={graphql`
+            query FooterQuery {
+                site {
+                    buildTime(fromNow: true)
+                  }
+                }
+          `}
+          render={data => (
+            <footer id="footer">
         <p className="copyright">&copy; {new Date().getFullYear()} Darin Levesque - Adapted from <OutboundLink href="https://html5up.net/dimension">Dimension</OutboundLink>. Built with:{" "}
             <OutboundLink href="https://www.gatsbyjs.org/">Gatsby.js</OutboundLink> Served with <OutboundLink href="https://www.netlify.com">Netlify</OutboundLink>
         </p>
+        <p className="copyright">Last Updated { data.site.buildTime }</p>
     </footer>
-)
+          )}
+        />
+      )
+// const Footer = (props) => (
+//     <footer
+//         id="footer"
+//         style={props.timeout
+//         ? {
+//             display: 'none'
+//         }
+//         : {}}>
+//         <p className="copyright">&copy; {new Date().getFullYear()} Darin Levesque - Adapted from <OutboundLink href="https://html5up.net/dimension">Dimension</OutboundLink>. Built with:{" "}
+//             <OutboundLink href="https://www.gatsbyjs.org/">Gatsby.js</OutboundLink> Served with <OutboundLink href="https://www.netlify.com">Netlify</OutboundLink>
+//             Last Updated: { data.site.buildTime }
+//         </p>
+//     </footer>
+// )
 
-Footer.propTypes = {
-    timeout: PropTypes.bool
-}
+// Footer.propTypes = {
+//     timeout: PropTypes.bool
+// }
 
-export default Footer
+// export default Footer
+
+// export const pageQuery = graphql`
+// query FooterQuery {
+//     site {
+//         buildTime(fromNow: true)
+//       }
+//     }
+//   `
