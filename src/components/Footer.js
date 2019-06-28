0e354617279db8db8c2d4364698b85e5e25e9ff8
Darin Levesque <darinlevesque@gmail.com>
2019-06-28 15:12:41 -0400

    corrected time ago in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,23 +2,26 @@ import React from 'react'
 import { StaticQuery, graphql } from "gatsby"
 // import PropTypes from 'prop-types'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
-
+import TimeAgo from 'react-timeago'
 
 export default () => (
         <StaticQuery
           query={graphql`
             query FooterQuery {
                 site {
-                    buildTime(fromNow: true)
+                    buildTime
+                    siteMetadata {
+                        author
+                      }
                   }
                 }
           `}
           render={data => (
             <footer id="footer">
-        <p className="copyright">&copy; {new Date().getFullYear()} Darin Levesque - Adapted from <OutboundLink href="https://html5up.net/dimension">Dimension</OutboundLink>. Built with:{" "}
+        <p className="copyright">&copy; {new Date().getFullYear()} { data.site.siteMetadata.author } - Adapted from <OutboundLink href="https://html5up.net/dimension">Dimension</OutboundLink>. Built with:{" "}
             <OutboundLink href="https://www.gatsbyjs.org/">Gatsby.js</OutboundLink> Served with <OutboundLink href="https://www.netlify.com">Netlify</OutboundLink>
         </p>
-        <p className="copyright">Last Updated { data.site.buildTime }</p>
+        <p className="copyright">Last Updated <TimeAgo date={ data.site.buildTime } /></p>
     </footer>
           )}
         />
