147b2ee2ed4b13c9308ab64759ed00a517e45c97
Darin Levesque <darinlevesque@gmail.com>
2019-07-07 00:19:58 -0400

    upgrade 404 page with gif & go home button

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
+import Link from 'gatsby-link'
+import error from '../images/error.gif'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404 error</h1>
+    <img src={error} />
+    <p>You just hit a route that doesn&#39;t exist...It's sickening.</p>
+    <Link to="/"><button>Go Home</button></Link>
   </Layout>
 )
 
