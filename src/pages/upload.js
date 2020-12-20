7dc20ae4ec76b2c44ed6af06b67915605367720f
Darin Levesque <darin@darinlevesque.com>
2020-12-20 10:25:46 -0500

    Added box upload widget

---
+++ src/pages/upload.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const UploadVideo = () => (
+  <Layout>
+    <iframe src="https://app.box.com/embed/s/7p7ace2yyfhmqu85t9v86u3n8vuthjxr?sortColumn=date&view=list&showParentPath=false" width="800" height="550" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>
+  </Layout>
+)
+
+export default UploadVideo
